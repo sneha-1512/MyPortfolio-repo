9fc25b4ea6febe3837307084af55b67d97a36bcc
unknown <ceccse.1902254@gmail.com>
2023-11-21 19:57:52 +0530

    first commit

---
+++ src/components/Experience/index.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import styled from 'styled-components'
+import ExperienceCard from '../Cards/ExperienceCard';
+import { experiences } from '../../data/constants';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    z-index: 1;
+    align-items: center;
+    padding: 40px 0px 80px 0px;
+    @media (max-width: 960px) {
+        padding: 0px;
+    }
+`;
+
+const Wrapper = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    max-width: 1350px;
+    padding: 80px 0;
+    gap: 12px;
+    @media (max-width: 960px) {
+        flex-direction: column;
+    }
+`;
+
+const Title = styled.div`
+font-size: 42px;
+text-align: center;
+font-weight: 600;
+margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+      margin-top: 12px;
+      font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+    font-size: 18px;
+    text-align: center;
+    max-width: 600px;
+    color: ${({ theme }) => theme.text_secondary};
+    @media (max-width: 768px) {
+        margin-top: 12px;
+        font-size: 16px;
+    }
+`;
+
+
+
+
+
+const index = () => {
+    return (
+        <Container id="experience">
+            <Wrapper>
+                <Title>Experience</Title>
+                <Desc>
+                    My work experience is as follows.
+                </Desc>
+                        {experiences.map((experience,index) => (
+                            
+                                    <ExperienceCard experience={experience}/>
+                               
+                        ))}
+                   
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default index
